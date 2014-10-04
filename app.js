b55f3d3a8f172115c105369428c5936f84c0e974
Daniel <dstovell@kabam.com>
2014-10-04 02:47:22 -0700

    Sectors, Factions, and Fleets, oh my!

--- app.js
+++ app.js
@@ -37,6 +37,58 @@ function printTitle() {
     console.log("                   \\/     \\/                 \\/        ");
 }
 
+var options = {};
+
+//Move this somewhere else later
+options.config = options.config || {
+    starmap: {
+        sectorCount: 1,
+        starCount: 40,
+        width: 2048,
+        height: 2048,
+        padding: 100,
+        minStarDistance: 250,
+        maxConnectionDistance: 400,
+        randomPositionAttempts:400,
+        starTypes: [
+            {id:'yellow', name:'Yellow Dwarf', color:'yellow', radius:10, img:'/game/starmap/Sun.png'},
+            {id:'red', name:'Red Giant', color:'red', radius:10, img:'/game/starmap/RedStar.png'},
+            {id:'blue', name:'Blue Giant', color:'blue', radius:10, img:'/game/starmap/BlueStar.png'}
+        ]
+    },
+
+    factions: {
+        npc:[
+            { name:'Feds', color:'green', starCount:10, contiguous:true },
+            { name:'Pirates', color:'red', starCount:5, contiguous:false},
+            { name:'Civillians', color:'yellow', starCount:0 }
+        ]
+    },
+
+    fleets: {
+        classes:[
+            {name:'Fighter'},
+            {name:'Shuttle'},
+            {name:'Corvette'},
+            {name:'Freighter'},
+            {name:'Capitol'},
+        ],
+
+        baseShips:[
+            { name:'Shuttle Pod',       warp:6, holds:5,    figs:50,    shields:50,     odds:0.7,   cost:0},
+            { name:'Merchant Cruiser',  warp:3, holds:75,   figs:2500,  shields:400,    odds:1.0,   cost:131695},
+            { name:'Scout Marauder',    warp:2, holds:25,   figs:150,   shields:100,    odds:2.0,   cost:27785},
+            { name:'Missile Frigate',  warp:3, holds:60,   figs:5000,  shields:400,    odds:1.3,   cost:146832},
+            { name:'BattleShip',        warp:4, holds:80,   figs:10000, shields:750,    odds:1.6,   cost:190236},
+            { name:'Corporate FlagShip',warp:3, holds:85,   figs:20000, shields:1500,   odds:1.2,   cost:352285},
+            { name:'Colonial Transport',warp:6, holds:250,  figs:200,   shields:500,    odds:0.6,   cost:711400},
+            { name:'CargoTran',         warp:4, holds:125,  figs:400,   shields:1000,   odds:0.8,   cost:226125},
+            { name:'Merchant Freighter',warp:2, holds:65,   figs:300,   shields:500,    odds:0.8,   cost:100015},
+            { name:'Imperial StarShip', warp:4, holds:150,  figs:50000, shields:2000,   odds:1.5,   cost:669290},
+        ]
+    }
+}
+
 exports.init = function (port) {
 
     printTitle();
@@ -73,7 +125,7 @@ exports.init = function (port) {
         next();
     });
 
-    var options = { db:db };
+    options.db = db;
 
     app.use('/', require('./routes/admin/index'));
     app.use('/admin', require('./routes/admin/index'));
--- routes/game/bridge.js
+++ routes/game/bridge.js
@@ -5,6 +5,7 @@ exports = module.exports = function routeSetup(options) {
 
     options.config = options.config || {
         starmap: {
+            sectorCount: 1,
             starCount: 40,
             width: 2048,
             height: 2048,
@@ -21,26 +22,93 @@ exports = module.exports = function routeSetup(options) {
     }
 
     var starmap_controller = require('../../lib/starmap_controller').create(options);
+    var faction_controller = require('../../lib/faction_controller').create(options);
+    var fleet_controller = require('../../lib/fleet_controller').create(options);
 
     router.get('/', function(req, res) {
         var uid = req.cookies.uid;
+        var username = req.cookies.username;
+
+        function getOrAddFaction(uid, cb) {
+            faction_controller.getUserFaction(uid, function (err, faction) {
+                if (err != null) {
+                   return cb(err);
+                }
+
+                if (faction != null) {
+                    return cb(null, faction);
+                }
+
+                var factionConfig = {name:username, color:'cyan'};
+
+                faction_controller.addUserFaction(uid, factionConfig, function (err, faction) {
+                    if (err != null) {
+                       return cb(err);
+                    }
+
+                    return cb(null, faction);
+                });
+            });
+        }
+
+        function getOrAddFleet(uid, faction, cb) {
+            fleet_controller.getFactionFleet(faction._id, function (err, fleet) {
+                if (err != null) {
+                   return cb(err);
+                }
+
+                if (fleet != null) {
+                    return cb(null, fleet);
+                }
+
+                var ships = ['Shuttle Pod'];
+                var sid = 0;
+                var starId = 1;
+
+                console.log("getOrAddFleet faction=" + JSON.stringify(faction));
+
+                fleet_controller.addFactionFleet(faction, ships, sid, starId, function (err, fleet) {
+                    if (err != null) {
+                       return cb(err);
+                    }
+                    
+                    return cb(null, fleet);
+                });
+            });
+        }
 
         if ((uid == null) || (uid == '')) {
             res.render('login', { config:options.config, layout:'game_layout' });
         }
         else {
-            starmap_controller.generateNewMap({}, function (err, map) {
-                if (err != null) {
-                    //req.flash('error', err);
-                }
-     
-                //console.log("map=" + JSON.stringify(map));
-                res.render('bridge', { config:options.config, layout:'game_layout', map:map });
+            getOrAddFaction(uid, function (err, faction) {
+
+                getOrAddFleet(uid, faction, function (err, fleet) {
+
+                    starmap_controller.getSector(fleet.sid, function (err, sector) {
+                        if (err != null) {
+                            //req.flash('error', err);
+                        }
+             
+                        //console.log("sector=" + JSON.stringify(sector));
+                        res.render('bridge', { config:options.config, layout:'game_layout', map:sector.map, faction:faction, fleet:fleet });
+                    });
+                });
             });
         }
     });
 
-    
+    router.get('/bigbang', function(req, res) {
+
+        var bigBangData = {};
+        starmap_controller.bigBang(bigBangData, function (err, bigBangData) {
+            faction_controller.bigBang(bigBangData, function (err, bigBangData) {
+                fleet_controller.bigBang(bigBangData, function (err, bigBangData) {
+                    res.json(bigBangData);
+                });
+            });
+        });
+    });    
 
     return router;
 }
