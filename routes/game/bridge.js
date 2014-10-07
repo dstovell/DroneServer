26bd22bd21421f492b39719721c30c578aa615f6
Daniel <dstovell@kabam.com>
2014-10-07 00:02:25 -0700

    Remove old config hardcoded in bridge route

--- routes/game/bridge.js
+++ routes/game/bridge.js
@@ -4,24 +4,6 @@ var router = express.Router();
 
 exports = module.exports = function routeSetup(options) {
 
-    options.config = options.config || {
-        starmap: {
-            sectorCount: 1,
-            starCount: 40,
-            width: 2048,
-            height: 2048,
-            padding: 100,
-            minStarDistance: 250,
-            maxConnectionDistance: 400,
-            randomPositionAttempts:400,
-            starTypes: [
-                {id:'yellow', name:'Yellow Dwarf', color:'yellow', radius:10, img:'/game/starmap/Sun.png'},
-                {id:'red', name:'Red Giant', color:'red', radius:10, img:'/game/starmap/RedStar.png'},
-                {id:'blue', name:'Blue Giant', color:'blue', radius:10, img:'/game/starmap/BlueStar.png'}
-            ]
-        }
-    }
-
     var starmap_controller = require('../../lib/starmap_controller').create(options);
     var faction_controller = require('../../lib/faction_controller').create(options);
     var fleet_controller = require('../../lib/fleet_controller').create(options);
@@ -95,9 +77,11 @@ exports = module.exports = function routeSetup(options) {
                             if (err != null) {
                                 //req.flash('error', err);
                             }
+
+                            var serverTime = new Date().getTime();
              
                             //console.log("sector=" + JSON.stringify(sector));
-                            res.render('bridge', {  config:options.config, layout:'game_layout', 
+                            res.render('bridge', {  config:options.config, layout:'game_layout', serverTime:serverTime,
                                                     sector:sector, faction:faction, fleet:fleet, factionSectors:factionSectors });
                         });
                     });
