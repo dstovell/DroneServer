a2e25752548355229e189d5f34f525a45035a13b
Daniel <dstovell@kabam.com>
2014-10-07 23:33:42 -0700

    Put Config in its own file, yay

--- app.js
+++ app.js
@@ -39,68 +39,7 @@ function printTitle() {
 
 var options = {};
 
-//Move this somewhere else later
-options.config = options.config || {
-
-    cdn:'http://dl.dropboxusercontent.com/u/8864550/DroneServer/',
-
-    starmap: {
-        sectorCount: 1,
-        starCount: 40,
-        width: 2048,
-        height: 2048,
-        padding: 100,
-        minStarDistance: 250,
-        maxConnectionDistance: 400,
-        randomPositionAttempts:400,
-        starTypes: [
-            {id:'yellow', name:'Yellow Dwarf', color:'yellow', radius:8, img:'/game/starmap/Sun.png'},
-            {id:'red', name:'Red Giant', color:'red', radius:8, img:'/game/starmap/RedStar.png'},
-            {id:'blue', name:'Blue Giant', color:'blue', radius:8, img:'/game/starmap/BlueStar.png'}
-        ],
-
-        planetTypes: [
-            {id:'C', name:'Glacial', color:'white', radius:2, img:'/game/starmap/Eris.png'},
-            {id:'H', name:'Volcanic', color:'red', radius:2, img:'/game/starmap/Venus.png'},
-            {id:'K', name:'Desert', color:'brown', radius:2, img:'/game/starmap/Mars.png'},
-            {id:'L', name:'Mountain', color:'brown', radius:2, img:'/game/starmap/Mercury.png'},
-            {id:'M', name:'Terrestrial', color:'green', radius:3, img:'/game/starmap/Earth.png'},
-            {id:'O', name:'Oceanic', color:'blue', radius:3, img:'/game/starmap/Neptune.png'},
-            {id:'U', name:'Gas Giant', color:'green', radius:5, img:'/game/starmap/Jupiter.png'},
-        ]
-    },
-
-    factions: {
-        npc:[
-            { name:'Feds', color:'#007FFF', starCount:10, contiguous:true },
-            { name:'Pirates', color:'red', starCount:5, contiguous:false},
-            { name:'Civillians', color:'yellow', starCount:0 }
-        ]
-    },
-
-    fleets: {
-        classes:[
-            {name:'Fighter'},
-            {name:'Shuttle'},
-            {name:'Corvette'},
-            {name:'Freighter'},
-            {name:'Capitol'},
-        ],
-
-        baseShips:[
-            { name:'Shuttle Pod',       warp:6, holds:5,    figs:50,    shields:50,     odds:0.7,   cost:0},
-            { name:'Merchant Cruiser',  warp:3, holds:75,   figs:2500,  shields:400,    odds:1.0,   cost:131695},
-            { name:'Scout Marauder',    warp:2, holds:25,   figs:150,   shields:100,    odds:2.0,   cost:27785},
-            { name:'Missile Frigate',  warp:3, holds:60,   figs:5000,  shields:400,    odds:1.3,   cost:146832},
-            { name:'BattleShip',        warp:4, holds:80,   figs:10000, shields:750,    odds:1.6,   cost:190236},
-            { name:'Corporate FlagShip',warp:3, holds:85,   figs:20000, shields:1500,   odds:1.2,   cost:352285},
-            { name:'Colonial Transport',warp:6, holds:250,  figs:200,   shields:500,    odds:0.6,   cost:711400},
-            { name:'CargoTran',         warp:4, holds:125,  figs:400,   shields:1000,   odds:0.8,   cost:226125},
-            { name:'Merchant Freighter',warp:2, holds:65,   figs:300,   shields:500,    odds:0.8,   cost:100015},
-            { name:'Imperial StarShip', warp:4, holds:150,  figs:50000, shields:2000,   odds:1.5,   cost:669290},
-        ]
-    }
-}
+options.config = require('./config/global.json');
 
 exports.init = function (port) {
 
