f5326a28513b1b80726910f8f970b9d42e4e74fa
dstovell <ninja@Daniels-iMac.local>
2016-01-25 01:08:05 -0800

    view galaxy first pass

--- routes/admin/galaxy.js
+++ routes/admin/galaxy.js
@@ -8,8 +8,12 @@ exports = module.exports = function routeSetup(options) {
     var galaxy_controller = require('../../lib/galaxy/galaxy_controller').create(options);
 
     router.get('/viewgalaxy', function(req, res){
+        console.log("/viewgalaxy");
+
+        var galaxyConfig = galaxy_controller.getConfig();
+
         galaxy_controller.getGalaxy(function(err, galaxy){
-             res.render('viewgalaxy', {galaxy:galaxy});
+             res.render('viewgalaxy', {layout:'game_layout', galaxy:galaxy, config:galaxyConfig, helper:galaxy_controller.galaxyHelper});
         });
        
     });
