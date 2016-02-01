022a6631212393f677af40c13c0b72c29a9767dc
dstovell <ninja@Daniels-iMac.local>
2016-01-31 21:55:51 -0800

    Assets: add/get/getForUser/remove/setCourse/addMod/removeMod

--- routes/admin/assets.js
+++ routes/admin/assets.js
@@ -7,14 +7,58 @@ exports = module.exports = function routeSetup(options) {
 
     var assets_controller = require('../../lib/assets/assets_controller').create(options);
 
-  	 router.get('/add/:uid/:assetKey/:starId', function(req, res){
-        console.log("/add");
+  	router.get('/add/:uid/:assetKey/:starId', function(req, res){
         var uid = req.params.uid;
         var assetKey = req.params.assetKey;
         var starId = req.params.starId;
 
-        console.log("/add uid=" + uid);
-        assets_controller.addAsset(assetKey, uid, starId, options, function(err, asset){
+        assets_controller.addAsset(assetKey, uid, starId, {}, function(err, asset){
+            return res.json({err:err, result:asset});
+        });
+    });
+
+    router.get('/remove/:uid/:transponder', function(req, res){
+		var transponder = req.params.transponder;
+        var uid = req.params.uid;
+
+        assets_controller.removeAsset(transponder, uid, {}, function(err, asset){
+            return res.json({err:err, result:asset});
+        });
+    });
+
+	router.get('/getForUser/:uid', function(req, res){
+        var uid = req.params.uid;
+
+        assets_controller.getAssetsForUser(uid, {}, function(err, assets){
+            return res.json({err:err, result:assets});
+        });
+    });
+
+	router.get('/get/:uid/:transponder', function(req, res){
+        var transponder = req.params.transponder;
+        var uid = req.params.uid;
+
+        assets_controller.getAsset(transponder, uid, {}, function(err, asset){
+            return res.json({err:err, result:asset});
+        });
+    });
+
+    router.get('/addMod/:uid/:transponder/:modKey', function(req, res){
+        var transponder = req.params.transponder;
+        var uid = req.params.uid;
+        var modKey = req.params.modKey;
+
+        assets_controller.addAssetMod(transponder, uid, modKey, {}, function(err, asset){
+            return res.json({err:err, result:asset});
+        });
+    });
+
+    router.get('/removeMod/:uid/:transponder/:modKey', function(req, res){
+        var transponder = req.params.transponder;
+        var uid = req.params.uid;
+        var modKey = req.params.modKey;
+
+        assets_controller.removeAssetMod(transponder, uid, modKey, {}, function(err, asset){
             return res.json({err:err, result:asset});
         });
     });
@@ -24,7 +68,7 @@ exports = module.exports = function routeSetup(options) {
         var transponder = req.params.transponder;
         var destStarId = req.params.destStarId;
 
-        assets_controller.setAssetCourse(transponder, uid, destStarId, options, function(err, asset){
+        assets_controller.setAssetCourse(transponder, uid, destStarId, {}, function(err, asset){
             return res.json({err:err, result:asset});
         });
     });
